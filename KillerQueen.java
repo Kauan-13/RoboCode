9e80cce690d8ea97feca688053fdbd0f6b5804a4
Renata Remour <github@remor.slmail.me>
2024-07-12 22:21:43 -0300

    Atualizando movimentação inicial do robô e verificando se a movimentação foi executada antes de realizar outras ações.

--- KillerQueen.java
+++ KillerQueen.java
@@ -13,6 +13,7 @@ public class KillerQueen extends Robot
 	 * run: KillerQueen's default behavior
 	 */
 	double tamanho;
+	boolean executou = false;
 	
 	public void run() {
 		// Initialization of the robot should be put here
@@ -26,8 +27,49 @@ public class KillerQueen extends Robot
 		
 		double alturaMapa = getBattleFieldHeight();
 		double larguraMapa = getBattleFieldWidth();	
+		double up = alturaMapa - getY();
+		double down = getY();
+		double left = getX();
+		double right = larguraMapa - getX();
+		double[] wallDistance = {up, down, right, left};
+		double menorDistancia = wallDistance[0];
+		int direcao = 0;
 
-		turnLeft(getHeading());
+
+// se robo = posição 10x em um mapa de 50x e 15y em um mapa de 35y 
+
+		for (int i = 0; i<4; i++){
+			if (wallDistance[i] < menorDistancia){
+				menorDistancia = wallDistance[i] ;
+				direcao = i;
+			}
+		}
+		
+		switch (direcao){
+			case 0:
+				turnLeft(getHeading() + 20);
+				ahead(up - 45);
+				executou = true;
+				break;
+			case 1:
+				turnLeft(getHeading() - 160);
+				ahead(down - 45);
+				executou = true;
+				break;
+			case 2:
+				turnLeft(getHeading() + 250);
+				ahead(right - 45);
+				executou = true;
+				break;
+			case 3:
+				turnLeft(getHeading() - 250);	
+				ahead (left - 45);
+				executou = true;
+				break;
+		}
+			
+
+				
 		
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
@@ -50,19 +92,21 @@ public class KillerQueen extends Robot
 		// Replace the next line with any behavior you would like
 		tamanho = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
 
-		if(e.getDistance() < tamanho/5 && getEnergy() > 50){
-			fire(3);
-			turnRight(e.getBearing());
-			scan();
-		}else if(e.getDistance() < tamanho/4 && getEnergy() > 30){
-			fire(2.5);
-			turnRight(e.getBearing());
-			scan();
-		}else if(e.getDistance() < tamanho/3 && getEnergy() > 10){
-			fire(2);
-			turnRight(e.getBearing());
-		}else{
-			fire(1.5);
+		if (executou){
+			if(e.getDistance() < tamanho/5 && getEnergy() > 50){
+				fire(3);
+				turnRight(e.getBearing());
+				scan();
+			}else if(e.getDistance() < tamanho/4 && getEnergy() > 30){
+				fire(2.5);
+				turnRight(e.getBearing());
+				scan();
+			}else if(e.getDistance() < tamanho/3 && getEnergy() > 10){
+				fire(2);
+				turnRight(e.getBearing());
+			}else{
+				fire(1.5);
+			}
 		}
 	}
 
@@ -83,7 +127,8 @@ public class KillerQueen extends Robot
 		
 		double alturaMapa = getBattleFieldHeight();
 		double larguraMapa = getBattleFieldWidth();	
-
+		
+		
 		if(getX() <= 24){  //Bate na esquerda
 			turnLeft(getHeading() - 90);
 			ahead(larguraMapa/4);
@@ -96,6 +141,7 @@ public class KillerQueen extends Robot
 		}else if(getY() >= alturaMapa - 24){ //Bate em cima
 			turnLeft(getHeading() + 180);
 			ahead(alturaMapa/4);
+
 		}
 	}
 }
